527397e2c0e018a80c850933815e12cd9f8cda49
Hoang Nguyen <hoang.nguyenvan@oivan.com>
2023-10-28 17:07:52 +0700

    Refactor directory for all Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HoangToniComponent } from './hoang-toni/hoang-toni.component';
-import { ParentComponent } from './components/parent/parent.component';
-import { ChildComponent } from './components/child/child.component';
-import { EncapsulationComponent } from './components/encapsulation/encapsulation.component';
-import { NoneEncapsulationComponent } from './components/none-encapsulation/none-encapsulation.component';
-import { EmulatedEncapsulationComponent } from './components/emulated-encapsulation/emulated-encapsulation.component';
-import { ShadowDomEncapsulationComponent } from './components/shadow-dom-encapsulation/shadow-dom-encapsulation.component';
+import { HoangToniComponent } from './components/hoang-toni/hoang-toni.component';
+import { ParentComponent } from './components/life-cycle/parent/parent.component';
+import { ChildComponent } from './components/life-cycle/child/child.component';
+import { EncapsulationComponent } from './components/encapsulation/encapsulation-root/encapsulation.component';
+import { NoneEncapsulationComponent } from './components/encapsulation/none-encapsulation/none-encapsulation.component';
+import { EmulatedEncapsulationComponent } from './components/encapsulation/emulated-encapsulation/emulated-encapsulation.component';
+import { ShadowDomEncapsulationComponent } from './components/encapsulation/shadow-dom-encapsulation/shadow-dom-encapsulation.component';
 
 @NgModule({
   declarations: [
