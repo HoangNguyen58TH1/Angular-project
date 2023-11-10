ef3bdb88ceed05956f28f82932ebb6501900dc1c
Hoang Nguyen <hoang.nguyenvan@oivan.com>
2023-11-10 22:56:20 +0700

    Angular - dynamic component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// dynamic component
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AdBannerComponent } from './app/components/dynamic-component/ad-banner.component';
+// bootstrapApplication(AdBannerComponent);
