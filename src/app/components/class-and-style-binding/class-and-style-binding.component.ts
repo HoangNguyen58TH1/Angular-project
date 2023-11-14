68aa8c48be93db0076cdf84e1e6c8e352a2ab617
Hoang Nguyen <46235937+HoangNguyen58TH1@users.noreply.github.com>
2023-11-14 22:03:36 +0700

    Angular - class && style binding (#8)
    Co-authored-by: Hoang Nguyen <hoang.nguyenvan@oivan.com>

---
+++ src/app/components/class-and-style-binding/class-and-style-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-and-style-binding',
+  templateUrl: './class-and-style-binding.component.html',
+  styleUrls: ['./class-and-style-binding.component.scss']
+})
+export class ClassAndStyleBindingComponent {
+  isBold: boolean = true; // 'any_value'
+  // else false == null == undefine == ''
+
+  textRed: string = "text-red bold"; // multi
+  textRedUnderline: Array<string> = ['text-red', 'bold', 'text-underline']; //array
+  bgColor = 'yellow'; // single style
+  multiStyle = 'color: pink; font-size: 32px'
+}
