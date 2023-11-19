301ab5f3a68e02b3449ba0a1a07f112270f961cd
Hoang Nguyen <hoang.nguyenvan@oivan.com>
2023-11-20 00:27:36 +0700

    Angular - Attribute Directive - self define

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'my-project';
-  currentItem = 'Television';
+  // currentItem = 'Television';
   // items = ['item1', 'item2', 'item3', 'item4'];
 
   // addItem(newItem: string) {
   //   this.items.push(newItem);
   // }
 
-  crossOffItem(item: string) {
-    console.warn(`Parent says: crossing off ${item}.`);
-  }
+  // crossOffItem(item: string) {
+  //   console.warn(`Parent says: crossing off ${item}.`);
+  // }
 }
